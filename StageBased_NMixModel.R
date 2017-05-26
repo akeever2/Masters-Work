7eb0175b40b7f0c484069df6246fa0110814da2f
akeever2 <allison.keever@cfc.umt.edu>
2017-05-26 09:05:48 -0600

    stage-based N mixture model
    Writing stage based N mixture model for white-tailed deer abundance
    estimates

--- StageBased_NMixModel.R
+++ StageBased_NMixModel.R
@@ -9,6 +9,151 @@
 
 ###################################################################################
 
+###################################################
+
+#         Make fake data                          #
+
+###################################################
+
+
+data.fn <- function (nsites=20, nprim=5, nocc=20, nstages=3, b0.lam=c(0.9,1.6,1.6), 
+                     b0.gam=c(0.1,-2.5,-2.5), b0.ome=c(.3,.9,.8), b0.p=c(.8,.8,.8)){
+  
+  # Definitions
+  
+    # nsites = number of sites
+    # nprim = number of primary periods
+    # nocc = number of secondary periods
+    # nstages = number of stages; fawn, doe, buck
+    # b0.lam = intercept for initial abundance for fawns, does, bucks
+    # b0.gam = intercept for recruitment for fawns, does, bucks
+    # b0.ome = intercept for apparent survival for fawns, does, bucks
+    # b0.p = intercept for detection probability for fawns, does, bucks
+  
+  # Arrays
+  
+    N <- array(dim=c(nsites, nstages, nprim))
+    y <- array(dim=c(nsites, nocc, nprim, nstages))
+    S <- array(dim=c(nsites, nstages, nprim))
+    G <- array(dim=c(nsites, nstages, nprim))
+    p <- array(dim=c(nstages))
+    
+    
+  # Initial abundance for primary period 1
+    
+    lam.f <- exp(b0.lam[1])
+    lam.d <- exp(b0.lam[2])
+    lam.b <- exp(b0.lam[3])
+    
+    for(i in 1:nsites){
+      N[i,1,1] <- rpois(1,lam.f) 
+      N[i,2,1] <- rpois(1,lam.d)
+      N[i,3,1] <- rpois(1,lam.b)
+    }
+    
+  # Apparent survival and recruitment
+    
+    ome.f <- 1 / (1 + exp(-b0.ome[1]))
+    ome.d <- 1 / (1 + exp(-b0.ome[2]))
+    ome.b <- 1 / (1 + exp(-b0.ome[3]))
+    
+    gam.f <-exp(b0.gam[1])
+    gam.d <-exp(b0.gam[2])
+    gam.b <-exp(b0.gam[3])
+    
+    for(i in 1:nsites){
+      for(t in 2){
+        S[i,1,t] <- rbinom(1, N[i,1,t-1], ome.f)
+        S[i,2,t] <- rbinom(1, N[i,2,t-1], ome.d)
+        S[i,3,t] <- rbinom(1, N[i,3,t-1], ome.b)
+        
+        G[i,1,t] <- rpois(1,gam.f * N[i,2,t-1])
+        G[i,2,t] <- rpois(1,gam.d * sum(N[i,,t-1]))
+        G[i,3,t] <- rpois(1,gam.b * sum(N[i,,t-1]))
+        
+        N[i,1,t] <- G[i,1,t]
+        N[i,2,t] <- round(0.5 * S[i,1,t]) + S[i,2,t] + G[i,2,t]
+        N[i,3,t] <- round(0.5 * S[i,1,t]) + S[i,3,t] + G[i,3,t]
+      }
+      
+      for(t in 3){
+        S[i,1,t] <- rbinom(1, N[i,1,t-1], ome.f)
+        S[i,2,t] <- rbinom(1, N[i,2,t-1], ome.d)
+        S[i,3,t] <- rbinom(1, N[i,3,t-1], ome.b)
+        
+        G[i,1,t] <- rpois(1,gam.f * N[i,2,t-1])
+        G[i,2,t] <- rpois(1,gam.d * sum(N[i,,t-1]))
+        G[i,3,t] <- rpois(1,gam.b * sum(N[i,,t-1]))
+        
+        N[i,1,t] <- S[i,1,t]
+        N[i,2,t] <- S[i,2,t] + G[i,2,t]
+        N[i,3,t] <- S[i,3,t] + G[i,3,t]
+      }
+      
+      for(t in 4){
+        S[i,1,t] <- rbinom(1, N[i,1,t-1], ome.f)
+        S[i,2,t] <- rbinom(1, N[i,2,t-1], ome.d)
+        S[i,3,t] <- rbinom(1, N[i,3,t-1], ome.b)
+        
+        G[i,1,t] <- rpois(1,gam.f * N[i,2,t-1])
+        G[i,2,t] <- rpois(1,gam.d * sum(N[i,,t-1]))
+        G[i,3,t] <- rpois(1,gam.b * sum(N[i,,t-1]))
+        
+        N[i,1,t] <- G[i,1,t]
+        N[i,2,t] <- round(0.5 * S[i,1,t]) + S[i,2,t] + G[i,2,t]
+        N[i,3,t] <- round(0.5 * S[i,1,t]) + S[i,3,t] + G[i,3,t]
+      }
+      
+      for(t in 5){
+        S[i,1,t] <- rbinom(1, N[i,1,t-1], ome.f)
+        S[i,2,t] <- rbinom(1, N[i,2,t-1], ome.d)
+        S[i,3,t] <- rbinom(1, N[i,3,t-1], ome.b)
+        
+        G[i,1,t] <- rpois(1,gam.f * N[i,2,t-1])
+        G[i,2,t] <- rpois(1,gam.d * sum(N[i,,t-1]))
+        G[i,3,t] <- rpois(1,gam.b * sum(N[i,,t-1]))
+        
+        N[i,1,t] <- S[i,1,t]
+        N[i,2,t] <- S[i,2,t] + G[i,2,t]
+        N[i,3,t] <- S[i,3,t] + G[i,3,t]
+      }
+    }
+    
+    for(s in 1:nstages){
+      p[s] <- 1 / (1 + exp(-b0.p[s]))
+
+      for(t in 1:nprim){
+        for(j in 1:nocc){
+          for(i in 1:nsites){
+            y[i,j,t,s] <- rbinom(1, N[i,s,t], p[s])
+          }
+        }
+      }
+    }
+    
+    
+    return(list(nsites=nsites, nstages=nstages, nocc=nocc, nprim=nprim, y=y,
+                N=N, b0.gam=b0.gam, b0.p=b0.p, b0.lam=b0.lam, b0.ome=b0.ome, 
+                G=G, S=S))
+    
+}
+
+
+datum <- data.fn(nsites=20, nprim=5, nocc=20, nstages=3, b0.lam=c(0.9,1.6,1.6), 
+                 b0.gam=c(0.1,-2.5,-2.5), b0.ome=c(.3,.9,.8), b0.p=c(.8,.8,.8))
+
+attach(datum)
+
+###################################################
+
+#         Run the model                           #
+
+###################################################
+
+
+library(R2jags)
+library(mcmcplots)
+
 
 sink("Pmodel.txt")
 cat("
@@ -20,15 +165,25 @@ cat("
         # apparent survival (ome), and detection (p) for stage s
 
         for(s in 1:nstages){
-          b0.lam[s] ~ ??
-          b0.gam[s] ~ ??
-          b0.ome[s] ~ ??
-          b0.p[s] ~ ??
+          b0.lam[s] ~ dnorm(0,0.001)
+          b0.gam[s] ~ dnorm(0,0.001)
+          b0.ome[s] ~ dnorm(0,0.001)
+          b0.p[s] ~ dnorm(0,0.001)
         }
   
       # Ecological process model
         
         # Initial abundance for primary period 1
+          
+          # Apparent survival and recruitment
+          
+              logit(omega[1]) <- b0.ome[1]
+              logit(omega[2]) <- b0.ome[2]
+              logit(omega[3]) <- b0.ome[3]
+              
+              log(gamma[1]) <- b0.gam[1]
+              log(gamma[2]) <- b0.gam[2]
+              log(gamma[3]) <- b0.gam[3]
 
         for(i in 1:nsites){
           log(lambda[i,1]) <- b0.lam[1]
@@ -41,20 +196,11 @@ cat("
 
 
           # Abundance for primary periods 2 through number of primary periods
-          
-          # Apparent survival and recruitment
-          for(t in 2:nprim){
-            logit(omega[1]) <- b0.ome[1]
-            logit(omega[2]) <- b0.ome[2]
-            logit(omega[3]) <- b0.ome[3]
 
+          for(t in 2:nprim){
             S[i,1,t] ~ dbin(omega[1], N[i,1,t-1])
             S[i,2,t] ~ dbin(omega[2], N[i,2,t-1])
             S[i,3,t] ~ dbin(omega[3], N[i,3,t-1])
-
-            log(gamma[1]) <- b0.gam[1]
-            log(gamma[2]) <- b0.gam[2]
-            log(gamma[3]) <- b0.gam[3]
 
             G[i,1,t] ~ dpois(gamma[1] * N[i,2,t-1])
             G[i,2,t] ~ dpois(gamma[2] * sum(N[i,,t-1]))
@@ -65,7 +211,7 @@ cat("
           # Abundance for fall periods (2 and 4)
           for(t in c(2,4)){
             N[i,1,t] <- G[i,1,t]
-            N[i,2,t] <- 0.5 * S[i,1,t] + S[i,2,t] + G[i,2,t]
+            N[i,2,t] <- rbin0.5 * S[i,1,t] + S[i,2,t] + G[i,2,t]
             N[i,3,t] <- 0.5 * S[i,1,t] + S[i,3,t] + G[i,3,t]
           }
           
@@ -80,11 +226,12 @@ cat("
 
         # Observation model
 
-        for(i in 1:nstage){
+        for(s in 1:nstages){
+          logit(p[s]) <- b0.p[s]
+          
           for(t in 1:nprim){
             for(j in 1:nocc){
-              for(i in 1:nsite){
-                logit(p[s]) <- b0.p[s]
+              for(i in 1:nsites){
                 y[i,j,t,s] ~ dbin(p[s], N[i,s,t])
               }
             }
@@ -96,20 +243,26 @@ cat("
     }",fill=TRUE)
 sink()
 
-win.data<-list(M=26, T=5, J=J, y=matrix(unlist((Models)),nrow=26))
+win.data<-list(y=y, nsites=nsites, nstages=nstages, nocc=nocc, nprim=nprim)
 
-Nst<-apply(Models, c(1,3), max)+1
+
+Nst <- apply(y, c(1,4,3), max, na.rm=TRUE)
 Nst[is.na(Nst)]<-1
-inits<-function(){list(N=rpois(20,25), B0.lam=rnorm(1,0,1), B0.r=rnorm(1,0,1), B0.p=rnorm(1,0,1))}
 
-params<-c("B0.lam", "B0.r", "B0.p", "Ntot", "fit", "fit.new")
+inits<-function(){list(N=Nst[,,1], b0.lam=rnorm(1,0,1), b0.gam=rnorm(1,0,1), 
+                       b0.p=rnorm(1,0,1), b0.ome=rnorm(1,0,1))}
+
+
+
+params<-c("b0.lam", "b0.gam", "b0.p", "b0.ome", "N", "S", "G")
 
 ni<-1000
 nt<-1
 nb<-50
 nc<-3
 
-out.P<-bugs(data=win.data,inits=inits, params,model.file="P_model.txt",n.thin=nt, n.chains=nc,n.burnin=nb, n.iter=ni,debug=TRUE,DIC=TRUE,working.directory=getwd())
+out.P<-jags(data=win.data,inits=inits, params,model.file="Pmodel.txt",n.thin=nt, 
+            n.chains=nc,n.burnin=nb, n.iter=ni)
 
 
 
